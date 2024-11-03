d3696b7d5284973cb0e9afd40bf71496d8ffdcd7
shiqi-11 <162114332+shiqiC-11@users.noreply.github.com>
2024-11-03 16:52:18 +0000

    Update HomeScreen.js
    test branch

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,7 +4,7 @@ import { View, Text, Button } from 'react-native';
 const HomeScreen = ({ navigation }) => {
   return (
     <View>
-      <Text>这是首页</Text>
+      <Text>这是首页 test branch</Text>
       <Button title="去设置页面" onPress={() => navigation.navigate('Settings')} />
     </View>
   );
